067cab17acfc17430d307c8583ab75e88c81f493
jihoo <jihoop0620@gmail.com>
2026-07-16 19:42:00 +0900

    update version

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 
 pkgs.stdenv.mkDerivation {
   pname = "hokkaido";
-  version = "0.16.1";
+  version = "0.17.0";
 
   src = ./.;
 
--- otaru/default.nix
+++ otaru/default.nix
@@ -11,7 +11,7 @@ in
 
 pkgs.rustPlatform.buildRustPackage {
   pname = "otaru";
-  version = "0.1.0";
+  version = "0.6.0";
 
   src = ./.;
 
